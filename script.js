27dd40d506a7438aade726719cc35fe8e89d7c8a
Layla <86987909+bl4nk-04@users.noreply.github.com>
2023-10-27 02:47:55 -0400

    Update script.js
    Added JS to have pop up appear on click of a button and added code to get to delete account page

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-document.getElementById("homescreenHeader").onclick = function () {
-    location.href = 'P_ClientHomeScreen.html';
-};
-
 document.getElementById("password").onclick = function () {
     location.href = 'password.html';
 };
@@ -17,3 +13,29 @@ document.getElementById("name").onclick = function () {
 document.getElementById("payment").onclick = function () {
     location.href = 'payment.html';
 };
+
+document.getElementById("delete").onclick = function () {
+    location.href = 'deleteAccount.html';
+};
+
+document.getElementById("backarrow").onclick = function () {
+    location.href = 'P_ClientHomeScreen.html';
+};
+
+// Get the elements by their ID
+var submitdelete = document.getElementById("submitdelete");
+var popupWindow = document.getElementById("popup-window");
+var confirmButton = document.getElementById("confirmButton");
+var cancelButton = document.getElementById("cancelButton");
+// Show the pop-up window when the link is clicked
+submitdelete.addEventListener("click", function (event) {
+    event.preventDefault();
+    popupWindow.style.display = "block";
+});
+// Hide the pop-up window when the close button is clicked
+confirmButton.addEventListener("click", function () {
+    popupWindow.style.display = "none";
+});
+cancelButton.addEventListener("click", function () {
+    popupWindow.style.display = "none";
+});
