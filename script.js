abe027ad27f0fd54d1d4eab634344858152ca90a
Layla <86987909+bl4nk-04@users.noreply.github.com>
2023-11-01 17:29:12 -0400

    Update script.js
    Removed a part which was preventing the logout button from function properly

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ document.getElementById("passwordmenu").onclick = function () {
     location.href = 'password.html';
 };
 
-document.getElementById("logout").onclick = function () {
-    location.href = 'homepage.html';
-};
-
 document.getElementById("password").onclick = function () {
     location.href = 'password.html';
 };
@@ -30,16 +26,19 @@ document.getElementById("delete").onclick = function () {
     location.href = 'deleteAccount.html';
 };
 
+// Pop up for deleting account
 // Get the elements by their ID
 var submitdelete = document.getElementById("submitdelete");
 var popupWindow = document.getElementById("popup-window");
 var confirmButton = document.getElementById("confirmButton");
 var cancelButton = document.getElementById("cancelButton");
+
 // Show the pop-up window when the link is clicked
 submitdelete.addEventListener("click", function (event) {
     event.preventDefault();
     popupWindow.style.display = "block";
 });
+
 // Hide the pop-up window when the close button is clicked
 confirmButton.addEventListener("click", function () {
     popupWindow.style.display = "none";
@@ -47,28 +46,30 @@ confirmButton.addEventListener("click", function () {
 cancelButton.addEventListener("click", function () {
     popupWindow.style.display = "none";
 });
+// End of pop up code
 
 //Log out function
-function logout(){
-    var reallyLogout=confirm("Do you really want to log out?");
-    if(reallyLogout){
-        location.href="homepage.html";
+function logout() {
+    var reallyLogout = confirm("Do you really want to log out?");
+    if (reallyLogout) {
+        location.href = "homepage.html";
     }
-    }
-    var answer = document.getElementById("logout");
-    if (answer.addEventListener) {
-        answer.addEventListener("click", logoutfunction, false);
-        } else {
-            answer.attachEvent('onclick', logoutfunction);
-    }  
+}
 
-    /*$(document).ready(function(){
-        $('.renewbtn').click(function(){
-            $(this).html($(this).html() == 'edit' ? 'modify' : 'edit');});
-    });*/
+var answer = document.getElementById("logout");
+if (answer.addEventListener) {
+    answer.addEventListener("click", logoutfunction, false);
+} else {
+    answer.attachEvent('onclick', logoutfunction);
+}
 
-    //Random Number Generator (temporary) for Serial Number
-    function RandomSN() {
-        var rnd = Math.floor(Math.random() * 1000000000);
-        document.getElementById('tb').value = rnd;
-    }
+/*$(document).ready(function(){
+    $('.renewbtn').click(function(){
+        $(this).html($(this).html() == 'edit' ? 'modify' : 'edit');});
+});*/
+
+//Random Number Generator (temporary) for Serial Number
+function RandomSN() {
+    var rnd = Math.floor(Math.random() * 1000000000);
+    document.getElementById('tb').value = rnd;
+}
