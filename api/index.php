037672d9d156ee6904708f99d15e0914e1cea28e
Transyfor <40695080+Transyfor@users.noreply.github.com>
2023-11-19 20:12:53 -0500

    testing

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@
     if($_SERVER["REQUEST_METHOD"]=== "POST"){ //So if the form below posts, this catches it
         //We start by connecting to the database by requiring our db_conn file
         $mysqli= require __DIR__ ."/db_conn.php";
-
+        echo '<script>alert("Found spot"); window.location.href="index.php";</script>';
         //Then we write the sql to select a record based on the email address
         $sql= sprintf("SELECT * FROM Users WHERE Username = '%s'",$mysqli->real_escape_string($_POST["user"])); //The sprintf function replaces the %s in our string with the the second parameter, which is the given username in this case.
 //We could've not written the real_escape_string part and just wrote $_POST["user"] directly, but real_escape_string protects us from attackers.
