0f5804ec78ebff92c58a4064e5c95edab70ee6a2
Layla <86987909+bl4nk-04@users.noreply.github.com>
2023-11-19 22:48:06 -0500

    Update script.js

--- api/script.js
+++ api/script.js
@@ -34,9 +34,9 @@ function logout() {
 
 var answer = document.getElementById("logout");
 if (answer.addEventListener) {
-  answer.addEventListener("click", logoutfunction, false);
+  answer.addEventListener("click", logout(), false);
 } else {
-  answer.attachEvent("onclick", logoutfunction);
+  answer.attachEvent("onclick", logout());
 }
 
 /*$(document).ready(function(){
