d93dc8b8ac8cae904dcba595e7672933709d073b
Layla <86987909+bl4nk-04@users.noreply.github.com>
2023-11-30 03:03:41 -0500

    Update license.php

--- license.php
+++ license.php
@@ -39,7 +39,7 @@ padding-top: 0%;">
         <div class="bubbleStyle" id="codediv">
             <form>
                 <h1 id="psswtitle">Redeem License Code</h1>
-                <label for="codeinput" id="code">Code:</label>
+                <label for="codeinput" id="code">Serial Number:</label>
                 <input type="text" id="codeinput" name="codeinput" class="inputBox">
                 <input type="submit" value="Submit" id="submitcode">
             </form>
