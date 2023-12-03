7e42b98a351c0770bd5817a94d558b5ab24b24a0
em-is-aligned <146028843+em-is-aligned@users.noreply.github.com>
2023-12-03 15:26:49 -0500

    Update deleteAccountClientHandler.php

--- deleteAccountClientHandler.php
+++ deleteAccountClientHandler.php
@@ -5,30 +5,30 @@ session_start(); // Start the session if not started already
 //Checks to make sure that the user inputs aren't empty
 if(empty($_POST["currentpassword"])) {
     // saying to print in the .php or the .html file what follows. If it's script, it'll excecute it.
-    echo '<script>alert("Must provide current password"); window.location.href="/deleteAccountClient.php";</script>';
+    echo '<script>alert("Must provide your password"); window.location.href="/deleteAccountClient.php";</script>';
     //Need to do exit(), as otherwise the php code will keep running even if the user goes to another page.
     exit();
 }
-
 
 //Next we need a variable that holds the info on how to connect to our database. I made a file for this already db_conn.php. This just accesses that and connectes to our database.
 $mysqli = require __DIR__ . "/db_conn.php"; // This is a connection object
 
 $password = $_POST['currentpassword'];
-$userID = $_SESSION['userID'];
-$delete = $_SESSION['Username']."deleted";
+ $userID = $_SESSION['userID'];
+ $delete = "deleted";
 
 if($password == $_SESSION['Password']) {
     // Use backticks for table and column names, and use prepared statements to prevent SQL injection
-    $sql = "UPDATE `Users` SET `Username` = ? WHERE `userID` = ?";
+    $sql = "UPDATE `Users` SET `Name` = ? WHERE `userID` = ?";
     if ($stmt = $mysqli->prepare($sql)) {
-        $stmt->bind_param("si", $delete, $userID); 
+        $stmt->bind_param("si", $delete, $userID);
         if ($stmt->execute()) {
             // Update session address if the query executed successfully
-            $_SESSION['Username'] = $delete;
-
+            $_SESSION['Name'] = $delete;
+            
             $successMessage = "Record updated successfully";
             header("Location: /index.php?success=" . urlencode($successMessage));
+            session_destroy();
             exit();
         } else {
             $errorMessage = "Error updating record: " . $mysqli->error;
@@ -44,7 +44,7 @@ if($password == $_SESSION['Password']) {
 }
 
 else {
-    echo '<script>alert("Incorect Old Password"); window.location.href="/deleteAccountClient.php";</script>';
+    echo '<script>alert("Incorect Old Email"); window.location.href="/deleteAccountClient.php";</script>';
     exit();
 }
 
